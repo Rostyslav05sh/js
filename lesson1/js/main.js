4b05a6c8365ea71ebd87c25f2d89b260de5b0419
Rostyslav-05 <shmagala05@gmail.com>
2023-11-21 19:04:09 +0200

    js.homework1

---
+++ lesson1/js/main.js
@@ -0,0 +1,34 @@
+let s1 = 'hello';
+let s2 = 'owu';
+let s3 = 'com';
+let s4 = 'ua';
+let a1 = 1;
+let a2 = 10;
+let a3= -999;
+let a4 = 123;
+let a5 = 3.14;
+let a6 = 2.7;
+let a7 = 16;
+let b1 = true;
+let b2 = false;
+console.log(s1,s2,s3,s4,a1,a2,a3,a4,a5,a6,a7,b1,b2)
+
+let firstName = 'Rostyslav';
+let middleName = 'Zenoviovych';
+let lastName = 'Shmahala';
+
+console.log(firstName + ' ' + middleName + ' ' + lastName )
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a, typeof b, typeof c)
+
+let yourName = prompt('Введіть ваше ім\'я:');
+let yourLastName = prompt('Введіть ваше По-Батькові:');
+let yourAge = prompt('Введіть ваш вік:');
+
+console.log(yourName)
+console.log(yourLastName)
+console.log(yourAge)
